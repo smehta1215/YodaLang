8d86dc368b685ea7c2d260f8b5820ac2cb7b4ca3
smehta1215 <smehta1215@gmail.com>
2016-12-02 17:18:00 -0600

    Variables completed, working on conditionals

---
+++ src/Test02.scala
@@ -0,0 +1,25 @@
+import yodalang.YodaLang
+
+object Test02 extends YodaLang {
+  def main(args: Array[String]) = {
+    Begin we will
+
+    Show me "Testing built in variable operations" you will
+
+    Force push "x" as 9 you will
+    Force push "y" as 4 you will
+    Force push "z" as 2.5 you will
+
+    // Make it support function expressions
+    Fear leads "x" anger 1.0
+    Anger leads "x" hate "y"
+    Hate leads "x" suffering 2
+    Suffering leads "x" darkness 4
+
+    Show me "'x' is equal to v|x" you will
+    Show me "'y' is equal to v|y" you will
+    Show me "'z' is equal to v|z" you will
+
+    Finish we will
+  }
+}
---
+++ src/Test03.scala
@@ -0,0 +1,34 @@
+import yodalang.YodaLang
+
+object Test03 extends YodaLang {
+  def main(args: Array[String]) = {
+    Begin we will
+
+    Show me "Conditional testing" you will
+
+     Jedi path (11 equal_to 10)
+      Show me "statement" you will
+      
+      Jedi path (3 equal_to 3)
+        Show me "statement7" you will
+      Sith path
+        Show me "statement8" you will
+      Close you will
+      
+      Show me "statement2" you will
+    Sith
+      Show me "statement3" you will
+      
+      Jedi path (3 equal_to 3)
+        Show me "statement9" you will
+      Sith
+        Show me "statement10" you will
+      Close you will
+      
+      Show me "statement6" you will
+    Close you will
+
+
+    Finish we will
+  }
+}
